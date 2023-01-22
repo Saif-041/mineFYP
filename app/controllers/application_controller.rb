49e509870b619fb164c7483fe0f935427096ad06
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2023-01-23 01:32:25 +0500

    update for 'Can't verify CSRF token authenticity.'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :cor
+    before_filter :cor
     def cor
       headers["Access-Control-Allow-Origin"]  = "*"
       headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
